075a38b614846366dfed0b9fdc1a281fd7d48b3b
SteffRhes <steffrhes@pm.me>
2025-01-31 12:06:08 +0100

    veld_infer.yaml: work in progress

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python:3.11.11
 RUN pip install notebook==7.3.2
 RUN pip install flair==0.15.0
+WORKDIR /veld/code/
+ENV FLAIR_CACHE_ROOT="/veld/input/models_cache/"
 
--- src/workflows/infer.py
+++ src/workflows/infer.py
@@ -1,6 +1,22 @@
+import json
+import os
+
+from flair.nn import Classifier
+from flair.data import Sentence
 
 
-text = None
+IN_FILE = "/veld/input/data/" + os.getenv("in_file")
+OUT_FILE = "/veld/output/" + os.getenv("out_file")
+DO_SPLIT_SENTENCES = os.getenv("do_split_sentences") == "true"
+# DO_INFER_NER = os.getenv("do_infer_ner") == "true"
+# DO_INFER_SENTIMENT = os.getenv("do_infer_sentiment") == "true"
+MODEL_INFER_NER = os.getenv("model_infer_ner")
+MODEL_INFER_SENTIMENT = os.getenv("model_infer_sentiment")
+MODEL_INFER_LINKS = os.getenv("model_infer_links")
+
+
+with open(IN_FILE, "r") as f:
+    text = f.read()
 
 
 def split_sentences():
@@ -11,11 +27,13 @@ def split_sentences():
 
 
 def infer_ner():
-    pass
+    classifier = Classifier.load(MODEL_INFER_NER)
+    classifier.predict(text)
 
 
 def infer_sentiment():
-    pass
+    classifier = Classifier.load(MODEL_INFER_SENTIMENT)
+    classifier.predict(text)
 
 
 def infer_links():
@@ -38,13 +56,26 @@ def infer_relations():
     pass
 
 
+def persist_inference_data():
+    global text
+    if type(text) is list:
+        text = [s.to_dict() for s in text]
+    elif type(text) is Sentence:
+        text = text.to_dict()
+    with open(OUT_FILE, "w") as f:
+        json.dump(text, f, indent=4, ensure_ascii=False)
+
+
 def main():
     if DO_SPLIT_SENTENCES:
         split_sentences()
-    if DO_INFER_NER:
+    if MODEL_INFER_NER is not None:
         infer_ner()
-    if DO_INFER_SENTIMENT:
+    if MODEL_INFER_SENTIMENT is not None:
         infer_sentiment()
+    if MODEL_INFER_LINKS is not None:
+        infer_links()
+    persist_inference_data()
 
 
 if __name__ == "__main__":
